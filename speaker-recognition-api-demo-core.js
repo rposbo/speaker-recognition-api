03936c19d096ea774edb1b1c51dd6fe8f3c41036
Robin Osborne <robin.osborne@gmail.com>
2017-11-21 13:25:36 +0000

    tidying the buttons, adding random lyrics

--- speaker-recognition-api-demo-core.js
+++ speaker-recognition-api-demo-core.js
@@ -1,12 +1,15 @@
 
 var thingsToRead = [
 	"Never gonna give you up\nNever gonna let you down\nNever gonna run around and desert you\nNever gonna make you cry\nNever gonna say goodbye\nNever gonna tell a lie and hurt you",
+	"There's a voice that keeps on calling me\n	Down the road, that's where I'll always be.\n	Every stop I make, I make a new friend,\n	Can't stay for long, just turn around and I'm gone again\n	\n	Maybe tomorrow, I'll want to settle down,\n	Until tomorrow, I'll just keep moving on.\n	\n	Down this road that never seems to end,\n	Where new adventure lies just around the bend.\n	So if you want to join me for a while,\n	Just grab your hat, come travel light, that's hobo style.",
+	"They're the world's most fearsome fighting team \n	They're heroes in a half-shell and they're green\n	When the evil Shredder attacks\n	These Turtle boys don't cut him no slack! \n	Teenage Mutant Ninja Turtles\nTeenage Mutant Ninja Turtles",
+	"If you're seein' things runnin' thru your head \n	Who can you call (ghostbusters)\n	An' invisible man sleepin' in your bed \n	Oh who ya gonna call (ghostbusters) \nI ain't afraid a no ghost \n	I ain't afraid a no ghost \n	Who ya gonna call (ghostbusters) \n	If you're all alone pick up the phone \n	An call (ghostbusters)",
 ];
 
 function enrollNewProfile(){
 	navigator.getUserMedia({audio: true}, function(stream){
 		console.log('I\'m listening... just start talking for a few seconds...');
-		console.log('Maybe read this: ' + thingsToRead[Math.floor(Math.random() * thingsToRead.length)]);
+		console.log('Maybe read this: \n' + thingsToRead[Math.floor(Math.random() * thingsToRead.length)]);
 		onMediaSuccess(stream, createProfile, 15);
 	}, onMediaError);
 }
@@ -21,6 +24,7 @@ function enrollNewVerificationProfile(){
 function startListeningForIdentification(){
 	if (profileIds.length > 0 ){
 		console.log('I\'m listening... just start talking for a few seconds...');
+		console.log('Maybe read this: \n' + thingsToRead[Math.floor(Math.random() * thingsToRead.length)]);
 		navigator.getUserMedia({audio: true}, function(stream){onMediaSuccess(stream, identifyProfile, 10)}, onMediaError);
 	} else {
 		console.log('No profiles enrolled yet! Click the other button...');
